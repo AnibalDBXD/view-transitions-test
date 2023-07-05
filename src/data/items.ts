dbb13a635d6e4ee0d930dcec0d5c5272c54bab11
Anibal <luis.olivares.externo@zeleri.com>
2023-07-05 14:57:27 -0400

    init project

---
+++ src/data/items.ts
@@ -0,0 +1,20 @@
+export const items = [
+  {
+    id: "1",
+    title: "Item 1",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "https://fastly.picsum.photos/id/19/200/300.jpg?hmac=znGSIxHtiP0JiLTKW6bT7HlcfagMutcHfeZyNkglQFM"
+  },
+  {
+    id: "2",
+    title: "Item 2",
+    description: "Donec risus sem, sagittis ut convallis ac, tincidunt eu magna. Sed tempus euismod massa, quis mollis nisi faucibus at. Quisque quis venenatis lorem, non posuere turpis. Morbi in varius magna. Maecenas vulputate finibus arcu eu dictum. Vestibulum porta iaculis lorem sit amet tempor. Nulla at congue nibh. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque tempus mauris ac arcu volutpat tempor.",
+    image: "https://fastly.picsum.photos/id/982/200/300.jpg?hmac=xTKU9lOLW4f1_EmdynREJoYXZ7jXQUnwUNcYa6Nf4WA"
+  },
+  {
+    id: "3",
+    title: "Item 3",
+    description: "Sed sed dui eget ante facilisis ornare id sit amet sem. Nullam sit amet interdum diam, id bibendum orci. Vivamus blandit metus ligula, id tristique elit rutrum in. Phasellus venenatis elementum urna ut euismod. Pellentesque lobortis, neque quis tempus sodales, ex magna blandit ipsum",
+    image: "https://fastly.picsum.photos/id/454/200/300.jpg?hmac=wGXBDB3HURz2isRdLrgggeWdD1yO3rdX4B3-jlzRncg"
+  }
+] as const
---
+++ src/utils/getViewTransitionStyle.ts
@@ -0,0 +1,6 @@
+
+const getViewTransitionStyle = (id: string, name: string) => {
+  return `view-transition-name: ${name}-${id}`
+}
+
+export default getViewTransitionStyle
